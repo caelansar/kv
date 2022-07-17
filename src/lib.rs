650239b0d68d34ca91fe51da46821e7ab3cadb5f
caelansar <819711623@qq.com>
2022-07-17 21:53:54 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod error;
+mod pb;
+mod storage;
+
+pub use error::KvError;
+pub use pb::abi::*;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
